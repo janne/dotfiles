63cc6ac38ffd1838975d04a49ff151e8ffedd057
Jan Andersson <jan.andersson@gear.se>
2009-02-03 10:30:10 +0100

    Made some improvements to git prompt. ? : Untracked file ! : Changed file + : Added file

--- git_prompt.sh
+++ git_prompt.sh
@@ -1,10 +1,16 @@
-function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+function parse_untracked {
+  [[ $(git status 2> /dev/null | grep "Untracked files") ]] && echo "?"
+}
+function parse_changed_uncommited {
+  [[ $(git status 2> /dev/null | grep "Changes to be committed") ]] && echo "+"
+}
+function parse_changed {
+  [[ $(git status 2> /dev/null | grep "Changed but not updated") ]] && echo "!"
 }
 function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1]/"
 }
- 
+
 function proml {
   local       BLACK="\[\033[0;30m\]"
   local        BLUE="\[\033[0;34m\]"
@@ -12,6 +18,8 @@ function proml {
   local   LIGHT_RED="\[\033[1;31m\]"
   local       GREEN="\[\033[0;32m\]"
   local LIGHT_GREEN="\[\033[1;32m\]"
+  local        CYAN="\[\033[0;36m\]"
+  local  LIGHT_CYAN="\[\033[1;36m\]"
   local       WHITE="\[\033[1;37m\]"
   local  LIGHT_GRAY="\[\033[0;37m\]"
   local       RESET="\[\033[0m\]"
@@ -25,8 +33,7 @@ function proml {
   esac
  
 PS1="${TITLEBAR}\
-$BLUE[$RED\$(date +%H:%M)$BLUE]\
-$BLUE[$RED\u@\h:\w$GREEN\$(parse_git_branch)$BLUE]\n\
+$BLUE[$RED\u:\w$GREEN\$(parse_git_branch)$RED\$(parse_changed)\$(parse_untracked)$GREEN\$(parse_changed_uncommited)$BLUE]\
 $GREEN\$$RESET "
 PS2='> '
 PS4='+ '
