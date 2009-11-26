dd3bce762f4b196ffb3a9c52b3713c95adc3164a
Jan Andersson <jan.andersson@gear.se>
2009-11-26 19:14:10 +0100

    Clean up zsh options

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -15,20 +15,9 @@ HISTSIZE=1000
 SAVEHIST=1000
 REPORTTIME=10 # print elapsed time when more than 10 seconds
 
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
-
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
+setopt CORRECT # Correct spelling of commands
+setopt NO_LIST_BEEP # Do not beep on ambiguous completion
+setopt PROMPT_SUBST # $ expansion etc. in prompts
+setopt EXTENDED_GLOB # Extended globbing
 
 bindkey '^?' backward-delete-char
