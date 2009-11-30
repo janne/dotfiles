01c1a4e08956c5d1c1aa47a597297c5db47c1f88
Jan Andersson <jan.andersson@gear.se>
2009-11-30 22:04:25 +0100

    Clean up in alias file

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,9 @@
-alias vi=mvim
-alias vim=mvim
-alias -g L="| less"
-alias T="tail -f"
-alias -s rb=vim
+# Commands
 alias ls="ls -G"
+alias vi="mvim"
+alias vim="mvim"
+alias t="tail -f"
+
+# Globals
+alias -g L="| less"
+alias -g G="| grep -"
