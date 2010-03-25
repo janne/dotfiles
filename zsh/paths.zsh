3dc6211c218e1ace881998b27f10944aa86f44a8
Jan Andersson <jan.andersson@travelpartner.se>
2010-03-25 20:10:25 +0100

    Added local ruby gems to PATH

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,2 +1,2 @@
-PATH="/usr/local/bin:/usr/local/mysql/bin:$HOME/bin:$PATH"
+PATH="$HOME/.gem/ruby/1.8/bin:/usr/local/bin:/usr/local/mysql/bin:$HOME/bin:$PATH"
 MANPATH="/usr/local/man:$HOME/man:$MANPATH"
